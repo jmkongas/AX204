95c8ab7f6b6ed6d67936071d155cc75e1a88a3e1
jmkongas <aaronkongsebastian@gmail.com>
2016-11-23 19:38:32 +0800

    sprite

--- Game1/app.js
+++ Game1/app.js
@@ -7,6 +7,8 @@ function preload() {
 	game.load.image('sky','assets/sky.png');
 	game.load.image('ground', 'assets/platform.png');
 	game.load.image('star', 'assets/star.png');
+
+	//3rd and 4th parameters are width and height respectively
 	game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
 }
 
@@ -23,13 +25,17 @@ function create() {
 
 	//  Scale it to fit the width of the game
 	ground.scale.setTo(2, 2);
+	//This stops it from falling away when you jump on it
 	ground.body.immovable = true;
 
-	// Ledges
+	// 2 extra ledges
 	var ledge = platforms.create(400, 400, 'ground');
 	ledge.body.immovable = true;
 	ledge = platforms.create(-150, 250, 'ground');
 	ledge.body.immovable = true;
+
+	//create a player
+	player = game.add.sprite(32, game.world.height - 150, 'dude')
 
 }
 

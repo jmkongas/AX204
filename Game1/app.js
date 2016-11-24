786c243cf5ea9def44cd241c66c4b59d0efde24e
jmkongas <aaronkongsebastian@gmail.com>
2016-11-24 14:57:45 +0800

    platforms fix

--- Game1/app.js
+++ Game1/app.js
@@ -17,7 +17,7 @@ function create() {
 	game.add.sprite(0, 0, 'sky');
 
 	// Making group of platforms
-	var platforms = game.add.physicsGroup();
+	platforms = game.add.physicsGroup();
 	platforms.enableBody = true;
 
 	// Ground

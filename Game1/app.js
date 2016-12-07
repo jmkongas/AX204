85bc1f91d295ca56425f97eb861b0cbdeae14797
jmkongas <aaronkongsebastian@gmail.com>
2016-12-07 13:24:42 +0800

    updatesss

--- Game1/app.js
+++ Game1/app.js
@@ -185,6 +185,8 @@ function update() {
 	game.physics.arcade.collide(stars, platforms);
 	//the function "collectStar" will be called whenever the player walks over the stars
 	game.physics.arcade.overlap(player, stars, collectStar, null, this);
+	game.physics.arcade.overlap(player, enemy1, lossScore1, null, this);
+	game.physics.arcade.overlap(player, enemy2, lossScore2, null, this);
 
 	// Resets player sprite speed
 	player.body.velocity.x = 0;
@@ -241,4 +243,15 @@ function update() {
 	    //  This just gives each star a slightly random bounce value
 	    star.body.bounce.y = 0.7 + Math.random() * 0.2;
 	}
+
+	function loseScore1 (player,enemy1){
+		score-=5;
+		scoreText.text = "Score: " +score;
+	}
+
+	function loseScore2 (player,enemy2){
+		score-=5;
+		scoreText.text = "Score: " +score;
+	}
+
 }
